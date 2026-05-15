264c1a0417008cc8daa1765f0c347e8504af0163
Ben <bdwells1996@gmail.com>
2026-05-15 14:56:18 +0100

    Fix: Nav tweak.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -30,7 +30,7 @@ function SimpleIcon({
 
 function Nav() {
 	return (
-		<nav className="fixed top-0 w-full nav-bg flex items-center justify-center py-[26px] px-[18px] z-100">
+		<nav className="fixed top-0 w-full nav-bg flex items-center justify-center py-[26px] pl-[18px] pr-[22px] z-100">
 			<div className="max-w-[1440px] w-full h-full flex justify-between items-center relative">
 				<div className="flex items-center gap-3.5">
 					<MobileMenu />
@@ -64,10 +64,7 @@ function Nav() {
 									size={14}
 									className="hidden lg:block"
 								/>
-								<a
-									href={link.href}
-									className="text-body-md lg:text-body-lg xl:text-body-xl"
-								>
+								<a href={link.href} className="text-body-lg xl:text-body-xl">
 									{link.label}
 								</a>
 							</li>
