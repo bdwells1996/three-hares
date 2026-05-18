0a8b4e07c2958ec3baa94a1e00f7f8072ee09c5f
Ben <bdwells1996@gmail.com>
2026-05-18 13:33:56 +0100

    Fix:

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,4 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-const appId = 
-
 export default defineCliConfig({ api: { projectId, dataset } })
